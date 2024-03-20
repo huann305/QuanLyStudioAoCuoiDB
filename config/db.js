26699d869932b57f1167d1f81ba819d9394074d7
huann305 <134941555+huann305@users.noreply.github.com>
2024-03-20 18:15:46 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect MongoDB at default port 27017.
-mongoose.connect('mongodb://localhost:27017/DatabaseASM', {
+mongoose.connect('mongodb://localhost:27017/DatabaseXuong', {
     useNewUrlParser: true,
 }).then(() => {
     console.log('Successfully connected to MongoDB.');
