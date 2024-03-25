bea264bf4032c62983de0eb56d8f598f4ccd8835
huann305 <bahuan305@gmail.com>
2024-03-25 17:36:18 +0700

    update login

--- controllers/AccountCtrl.js
+++ controllers/AccountCtrl.js
@@ -5,42 +5,48 @@ const Employees = require('../models/Employees')
 const JWT = require('jsonwebtoken')
 require('dotenv').config();
 const SECRETKEY = process.env.TOKEN_SEC_KEY
+
 //login
-router.post('/login', async function(req, res, next) {
+router.post('/login', async function (req, res, next) {
     try {
         const username = req.body.username
         const password = req.body.password
-        const account = await Employees.findOne({username: username, password: password})
+        const account = await Employees.findOne({ username: username, password: password })
         if (account) {
             res.status(200).json(account)
         } else {
             res.status(404).json(null)
         }
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
-router.get('/loginauth', async (req, res) => {
+router.get('/login', async (req, res) => {
     try {
         const username = req.body.username
         const password = req.body.password
-        const account = await Employees.findOne({username: username, password: password})
+        const account = await Employees.findOne({ username: username})
         if (account) {
-            const token = JWT.sign({ id: account._id }, SECRETKEY, { expiresIn: '1h' })
-            const refreshToken = JWT.sign({ id: account._id }, SECRETKEY, { expiresIn: '1d' })
-            res.json({
-                "status": 200,
-                "messenger": "Đăng nhâp thành công",
-                "data": account,
-                "token": token,
-                "refreshToken": refreshToken
-            })
-        } else {
+            if (account.password === password) {
+                account.password = undefined
+                const token = JWT.sign({ id: account._id }, SECRETKEY, { expiresIn: '1h' })
+                res.json({
+                    "status": 200,
+                    "messenger": "Đăng nhâp thành công",
+                    "data": account,
+                    "token": token
+                })
+            } else {
+                res.json({
+                    "status": 400,
+                    "messenger": "Lỗi, Sai mật khẩu",
+                })
+            }
+        }else{
             res.json({
                 "status": 400,
-                "messenger": "Lỗi, đăng nhập không thành công",
-                "data": []
+                "messenger": "Không tìm thấy tài khoản",
             })
         }
 
@@ -50,7 +56,7 @@ router.get('/loginauth', async (req, res) => {
 })
 
 //register
-router.post('/register', async function(req, res, next) {
+router.post('/register', async function (req, res, next) {
     try {
         const newAccount = new Employees(req.body);
         const saveAccount = await newAccount.save();
--- controllers/EmployeeTaskCtrl.js
+++ controllers/EmployeeTaskCtrl.js
@@ -12,4 +12,57 @@ router.get('/tasksnemployee', async function(req, res, next) {
     }
 })
 
+//add employee task
+router.post('/tasksnemployee', async function(req, res, next) {
+    try {
+        const newEmployeeTask = new EmployeeTask(req.body);
+        const saveEmployeeTask = await newEmployeeTask.save();
+        res.status(200).json(saveEmployeeTask);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//update employee task
+router.put('/tasksnemployee/:id', async function(req, res, next) {
+    try {
+        const updateTask = await EmployeeTask.findByIdAndUpdate(req.params.id, req.body).populate('idEmployee').populate('idTask');
+        if(updateTask) {
+            res.status(200).json(updateTask);
+        }else {
+            res.status(404).json({ message: "Task not found" });
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//get employee task by id
+router.get('/tasksnemployee/:id', async function(req, res, next) {
+    try {
+        const task = await EmployeeTask.findOne({ _id: req.params.id }).populate('idEmployee').populate('idTask');
+        if(task) {
+            res.json(task);
+        }else {
+            res.status(404).json({ message: "Task not found" });
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//delete employee task
+router.delete('/tasksnemployee/:id', async function(req, res, next) {
+    try {
+        const customers = await EmployeeTask.findByIdAndDelete(req.params.id);
+        if(customers) {
+            res.status(200).json(customers);
+        }else {
+            res.status(404).json({ message: "Task not found" });
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 module.exports = router
