2929b469b2d3a4667abf64a04b656f384d2023a5
Hoang Yen <yenphph34781@fpoly.edu.vn>
2024-04-01 14:08:16 +0700

    HoangYen

--- controllers/Service.js
+++ controllers/Service.js
@@ -3,29 +3,29 @@ const router = express.Router();
 const Upload = require('../config/common/upload')
 //addService
 const Services = require('../models/services')
-// router.post('/services', async function(req, res, next) {
-//     try {
-//         const newService = new Services(req.body);
-//         const saveService = await newService.save();
-//         res.status(200).json(saveService);
-//     } catch (error) {
-//         res.status(500).json(error);
-//     }
-// });
-
-//add service with image
-router.post('/services', Upload.single('image'), async function(req, res, next) {
+router.post('/services', async function(req, res, next) {
     try {
         const newService = new Services(req.body);
-        const {file} = req
-        const urlImage = `${req.protocol}://${req.get('host')}/uploads/${file.filename}`
-        newService.image = urlImage
         const saveService = await newService.save();
         res.status(200).json(saveService);
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
+
+//add service with image
+// router.post('/services', Upload.single('image'), async function(req, res, next) {
+//     try {
+//         const newService = new Services(req.body);
+//         const {file} = req
+//         const urlImage = `${req.protocol}://${req.get('host')}/uploads/${file.filename}`
+//         newService.image = urlImage
+//         const saveService = await newService.save();
+//         res.status(200).json(saveService);
+//     } catch (error) {
+//         res.status(500).json(error);
+//     }
+// })
 
 //getService
 router.get('/services', async function(req, res, next){
@@ -45,30 +45,9 @@ router.get('/services/:id', async function(req, res, next){
     }
 });
 //Update Service
-// router.put('/services/:id', async function(req, res, next){
-//     try {
-//         const result = await Services.findByIdAndUpdate(req.params.id, req.body);
-//         if(!result) {
-//             res.status(404).json({
-//                 message: "Service not found"
-//             });
-//         } else {
-//             res.status(200).json({
-//                 message: "Service updated successfully",
-//                 data : result
-//             });
-//         }
-//     } catch (error) {
-//         res.status(500).json(error);
-//     }
-// });
-
-//update service with image
-router.put('/services/:id', Upload.single('image'), async function(req, res, next){
+router.put('/services/:id', async function(req, res, next){
     try {
-        const {file} = req
-        const urlImage = `${req.protocol}://${req.get('host')}/uploads/${file.filename}`
-        const result = await Services.findByIdAndUpdate(req.params.id, {...req.body, image: urlImage});
+        const result = await Services.findByIdAndUpdate(req.params.id, req.body);
         if(!result) {
             res.status(404).json({
                 message: "Service not found"
@@ -82,7 +61,28 @@ router.put('/services/:id', Upload.single('image'), async function(req, res, nex
     } catch (error) {
         res.status(500).json(error);
     }
-})
+});
+
+//update service with image
+// router.put('/services/:id', Upload.single('image'), async function(req, res, next){
+//     try {
+//         const {file} = req
+//         const urlImage = `${req.protocol}://${req.get('host')}/uploads/${file.filename}`
+//         const result = await Services.findByIdAndUpdate(req.params.id, {...req.body, image: urlImage});
+//         if(!result) {
+//             res.status(404).json({
+//                 message: "Service not found"
+//             });
+//         } else {
+//             res.status(200).json({
+//                 message: "Service updated successfully",
+//                 data : result
+//             });
+//         }
+//     } catch (error) {
+//         res.status(500).json(error);
+//     }
+// })
 
 
 //DelService
--- controllers/Statistics.js
+++ controllers/Statistics.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Bills = require('../models/Bills')
+const BillDetails = require('../models/BillDetails')
 
 //get revenue
 router.get('/revenue', async function(req, res, next) {
@@ -33,5 +34,27 @@ router.get('/sales', async function(req, res, next) {
         res.status(500).json({message: error.message})
     }
 })
+//tinh tong theo dv
+router.get('/totalServicePrice', async function(req, res, next) {
+    try {
+        const { startDate, endDate, idService } = req.query; 
+        console.log(startDate, endDate, idService);
+        const queryBill = {
+            date: { $gte: startDate, $lte: endDate } 
+        };
+        const bills = await Bills.find(queryBill);
 
+        let total = 0;
+     
+        for(let i = 0; i < bills.length; i++) {
+            const billDetails = await BillDetails.find({ idBill: bills[i]._id, idService: idService });
+            for(let j = 0; j < billDetails.length; j++) {
+                total += billDetails[j].price;
+            }
+        }
+        res.status(200).json(total);
+    } catch (error) {    
+        res.status(500).json({message: error.message});
+    }
+});
 module.exports = router
