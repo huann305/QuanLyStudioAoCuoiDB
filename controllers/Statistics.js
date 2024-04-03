3fe7a70e579936c3e0d060060f9269e1aa766aa3
Hoang Yen <yenphph34781@fpoly.edu.vn>
2024-04-04 01:02:24 +0700

    y

--- controllers/Statistics.js
+++ controllers/Statistics.js
@@ -68,7 +68,6 @@ router.get('/sales', async function (req, res, next) {
                 listBillDetails.push(billDetails[j])
             }
         }
-
         const listServiceDetails = []
         for (let i = 0; i < listIdServiceDetails.length; i++) {
             const serviceDetails = await ServiceDetails.findById(listIdServiceDetails[i])
@@ -76,44 +75,68 @@ router.get('/sales', async function (req, res, next) {
         }
 
         const aaa = []
-        for(let i = 0; i < services.length; i++) {
+        for (let i = 0; i < services.length; i++) {
             const test = await BillDetails.countDocuments({ idService: services[i]._id })
             //count idService in billDetails
             let count = 0
-            for(let j = 0; j < listServiceDetails.length; j++) {
-                if(listServiceDetails[j].idService == services[i]._id.toString()) {
+            for (let j = 0; j < listServiceDetails.length; j++) {
+                if (listServiceDetails[j].idService == services[i]._id.toString()) {
                     count += listBillDetails[j].price
                 }
             }
-            aaa.push({idService: services[i]._id, count: test, totalPrice: count})
+            aaa.push({ idService: services[i]._id, count: test, totalPrice: count })
         }
-        aaa.sort((a, b) => b.count - a.count)
-        res.status(200).json(aaa)
+        const kq = []
+
+        // const service = aaa.find(x => x.idService === service._id);
+        // aaa.filter(x => {
+        //     return kq.push({
+        //         idService: x.idService,
+        //         count: aaa[i].count,
+        //         totalPrice: aaa[i].totalPrice,
+        //         serviceName: service ? service.serviceName : ''
+        //     })
+        // })
+
+
+
+        for (let i = 0; i < aaa.length; i++) {
+            const service = await Services.findById(aaa[i].idService);
+            kq.push({
+                idService: aaa[i].idService,
+                count: aaa[i].count,
+                totalPrice: aaa[i].totalPrice,
+                serviceName: service ? service.serviceName : ''
+            });
+        }
+
+
+        res.status(200).json(kq);
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 })
 //tinh tong theo dv
-router.get('/totalServicePrice', async function(req, res, next) {
+router.get('/totalServicePrice', async function (req, res, next) {
     try {
-        const { startDate, endDate, idService } = req.query; 
+        const { startDate, endDate, idService } = req.query;
         console.log(startDate, endDate, idService);
         const queryBill = {
-            date: { $gte: startDate, $lte: endDate } 
+            date: { $gte: startDate, $lte: endDate }
         };
         const bills = await Bills.find(queryBill);
 
         let total = 0;
-     
-        for(let i = 0; i < bills.length; i++) {
+
+        for (let i = 0; i < bills.length; i++) {
             const billDetails = await BillDetails.find({ idBill: bills[i]._id, idService: idService });
-            for(let j = 0; j < billDetails.length; j++) {
+            for (let j = 0; j < billDetails.length; j++) {
                 total += billDetails[j].price;
             }
         }
         res.status(200).json(total);
-    } catch (error) {    
-        res.status(500).json({message: error.message});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 });
 module.exports = router
